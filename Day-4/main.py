707748429dd72f1c38d11fd22b5d573df449619a
Farhaan <farrow.aman@gmail.com>
2023-02-16 01:16:56 +0530

    day 4: random number generator

---
+++ Day-4/main.py
@@ -0,0 +1,11 @@
+import random
+
+# Random whole number
+# c = random.randint(1, 10)
+
+# Random float number
+# c = random.random()
+
+# Random float number between 0 and 5
+c = random.randint(0, 5) + random.random()
+print(c)
