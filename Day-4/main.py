14fec5ca0c956a10eafdcbff8c6ff66eb0e468aa
Farhaan <farrow.aman@gmail.com>
2023-02-19 02:02:06 +0530

    some more day 4 exercises

--- Day-4/main.py
+++ Day-4/main.py
@@ -7,5 +7,10 @@ import random
 # c = random.random()
 
 # Random float number between 0 and 5
-c = random.randint(0, 5) + random.random()
-print(c)
+# c = random.randint(0, 5) + random.random()
+# print(c)
+
+list_items = ["Thor", "Loki", "Kratos"]
+
+#  this will print last item of the list
+print(list_items[-1])
