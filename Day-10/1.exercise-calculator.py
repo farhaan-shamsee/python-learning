4f6c20ea589ef3a79120bd606865fd59a04347fc
Farhaan <farhaan39@gmail.com>
2023-03-23 00:54:52 +0530

    added Day 10 files

---
+++ Day-10/1.exercise-calculator.py
@@ -0,0 +1,61 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+# Add
+def add(n1, n2):
+    return n1 + n2
+
+# Subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+# Multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+# Divide
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+
+}
+
+print(logo)
+
+num1 = int(input("What is the first number? : "))
+
+for symbol in operations:
+    print(symbol)
+
+continue_calculation = True
+
+
+while continue_calculation:
+    operation_symbol = input("Pick an operation from the line above: ")
+    num2 = int(input("What is the next number? : "))
+    task = operations[operation_symbol]
+    answer = task(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: ") == "y":
+        num1 = answer
+    else:
+        continue_calculation = False
